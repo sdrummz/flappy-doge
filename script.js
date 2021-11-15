2250d5471e4d85b346206b6c2485c2586105dffd
szglabs <sdrummond95@gmail.com>
2021-11-14 23:32:25 -0500

    layout/starter code

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('keypress', handleStart, { once: true });
+const title = document.querySelector('[data-title');
+
+let lastTime;
+function updateLoop(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(updateLoop);
+    return;
+  }
+  const delta = time - lastTime;
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleStart() {
+  title.classList.add('hide');
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleLose() {}
